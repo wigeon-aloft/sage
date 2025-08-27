173dc0ef163b2df2692747cfbbe53809d083ccfb
Rajesh Agrawal <Rajesh.Agarawal@gmail.com>
2025-08-27 15:55:32 +0530

    Implement GTK builder

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gotk3/gotk3/gtk"
 	"gitlab.wige.one/wigeon/sage/internal/ui"
 )
 
@@ -13,6 +14,7 @@ const APP_VERSION = "0.0.1"
 const DEFAULT_LAYOUT_PATH = "internal/ui/layout/sage.xml"
 
 func main() {
+	gtk.Init(nil)
 
 	// application, err := gtk.ApplicationNew(APP_ID, glib.APPLICATION_FLAGS_NONE)
 	//
@@ -28,13 +30,41 @@ func main() {
 	//
 	// })
 	//
-
+	
+	// Create your application via your custom UI wrapper
 	application, err := ui.ApplicationNew(APP_ID, APP_NAME, APP_VERSION)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	exitCode, err := application.Start()
-	application.Exit(exitCode)
+	// Load the layout via GTK Builder
+	builder, err := gtk.BuilderNewFromFile(DEFAULT_LAYOUT_PATH)
+	if err != nil {
+		log.Fatalf("Unable to load UI file: %v", err)
+	}
 
+	// Get the main window defined in your sage.xml
+	obj, err := builder.GetObject("main_window") // <-- ID must match sage.xml
+	if err != nil {
+		log.Fatalf("Could not get main_window: %v", err)
+	}
+
+	win, ok := obj.(*gtk.Window)
+	if !ok {
+		log.Fatal("main_window is not a *gtk.Window")
+	}
+
+	// Attach window close event
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	win.ShowAll()
+
+	// Start your application
+	exitCode, err := application.Start()
+	if err != nil {
+		log.Fatalf("Application failed: %v", err)
+	}
+	application.Exit(exitCode)
 }
